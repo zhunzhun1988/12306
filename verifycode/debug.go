ae0d4d6064d56216f3ad148df84e82605f07a49d
patrick <635457511@qq.com>
2017-09-11 17:57:00 +0800

    add opencv

--- verifycode/debug.go
+++ verifycode/debug.go
@@ -1,6 +1,7 @@
 package verifycode
 
 import (
+	"12306/opencv"
 	"fmt"
 )
 
@@ -31,6 +32,40 @@ func checkIndexToPos(image string, i, j int) (x, y int) {
 }
 
 func (dv *DebugVerify) GetAnswer(imagepath string) VerifyPosList {
+
+	//_, currentfile, _, _ := runtime.Caller(0)
+	//filename := path.Join(path.Dir(currentfile), imagepath)
+
+	image := opencv.LoadImage(imagepath)
+	if image == nil {
+		panic("LoadImage test.jpg fail  ")
+	}
+	defer image.Release()
+
+	mat := image.GetMat()
+	for i := 0; i < mat.Rows(); i++ {
+		for j := 0; j < mat.Cols(); j++ {
+			s := mat.Get2D(i, j)
+			fmt.Sprintf("%d ", s.Val()[0])
+		}
+	}
+
+	win := opencv.NewWindow("Go-OpenCV")
+	defer win.Destroy()
+
+	win.SetMouseCallback(func(event, x, y, flags int) {
+		//fmt.Printf("event = %d, x = %d, y = %d, flags = %d\n",
+		//	event, x, y, flags,
+		//)
+	})
+	win.CreateTrackbar("Thresh", 1, 100, func(pos int) {
+		fmt.Printf("pos = %d\n", pos)
+	})
+
+	win.ShowImage(image)
+
+	opencv.WaitKey(0)
+
 	n := 0
 	var poss VerifyPosList
 	fmt.Printf("num:")
--- verifycode/interface.go
+++ verifycode/interface.go
@@ -5,6 +5,58 @@ import (
 	"strings"
 )
 
+const (
+	Image_Size   = 67
+	Image_SpaceW = 5
+	Image_SpaceH = 5
+	Pic_HeadHigh = 36
+)
+
+func GetImageRowNum(w, h int) int {
+	h = h - Pic_HeadHigh - Image_SpaceH
+	if h > 0 {
+		ret := h / (Image_Size + Image_SpaceH)
+		if h%(Image_Size+Image_SpaceH) == 0 {
+			return ret
+		}
+		return ret + 1
+	}
+	return 0
+}
+
+func GetImageColNum(w, h int) int {
+	w = w - Image_SpaceW
+	if w > 0 {
+		ret := w / (Image_Size + Image_SpaceW)
+		if w%(Image_Size+Image_SpaceW) == 0 {
+			return ret
+		}
+		return ret + 1
+	}
+	return 0
+}
+
+func GetImageRange(picW, picH, indexR, indexC int) (startW, startH, endW, endH int) {
+	rows := GetImageRowNum(picW, picH)
+	cols := GetImageColNum(picW, picH)
+	if indexR <= 0 || indexC <= 0 || rows < indexR || cols < indexC {
+		return -1, -1, -1, -1
+	}
+	if indexC <= 1 {
+		startW = Image_SpaceW
+	} else {
+		startW = (indexC-1)*Image_Size + indexC*Image_SpaceW
+	}
+	if indexR <= 1 {
+		startH = Pic_HeadHigh + Image_SpaceH
+	} else {
+		startH = Pic_HeadHigh + (indexR-1)*Image_Size + indexR*Image_SpaceH
+	}
+	endW += startW + Image_Size
+	endH += startH + Image_Size
+	return
+}
+
 type VerifyPos struct {
 	X int
 	Y int
